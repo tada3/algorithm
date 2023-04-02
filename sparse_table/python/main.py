f4ad85de99dbeae0d8254657de4d6fd6722080cd
yoshihara <tadafumi.yoshihara@linecorp.com>
2023-04-02 23:22:42 +0900

    fixed comments.

--- sparse_table/python/main.py
+++ sparse_table/python/main.py
@@ -1,4 +1,4 @@
-# ここでは最小値の場合を実装する。最大値の場合もほぼ同じ。
+# Implement the 'minmium' case here, the case of 'maximum' is almost the same.
 class SparseTable:
 	def __init__(self, a):
 		self.a = a
@@ -7,19 +7,21 @@ class SparseTable:
 		log_size = self.logs[size] + 1
 		self.st = self.build_st(size, log_size)
 
-	# st[i][j]: [j, j + 2**i) の最小値に対応するaのindex
-	# 最小値そのものを値にしても良いが、他の値（ノードIDとか）が知りたい場合もあるので、indexにしておく
+	# st[i][j]: index corresponding to the minimum value in [j, j + 2**i) of a.
+	# Here we use half open interval.
+	# It is possible to set the minimum value to st[i][j], but we set an index because sometimes 
+	# we need to get other attributes such as node ID.
 	def build_st(self, n, m):
 		st = [ [0]*n for i in range(m) ]
 
-		# st[0][j]は[j, j+1)の解に対応するindex、すなわちj
+		# st[0][j] is an index of a that has the minimum value in [j, j+1).
 		for j in range(n):
 			st[0][j] = j
 		
 		for i in range(1, m):
 			for j in range(n):
 				end = j + (1 << i)
-				# endが範囲外の場合は必要ないので無視。
+				# Value is not needed if end is out of range.
 				if end > n:
 					break
 				st[i][j] = self.min_a(st[i-1][j], st[i-1][j+(1<<(i-1))])
@@ -30,7 +32,7 @@ class SparseTable:
 			return x
 		if x > y:
 			x, y = y, x
-		# 開区間にする [x, y)
+		# Make it half open interval.
 		y += 1
 		log_len = self.logs[y-x]
 		return self.min_a(self.st[log_len][x], self.st[log_len][y-(1<<log_len)])
